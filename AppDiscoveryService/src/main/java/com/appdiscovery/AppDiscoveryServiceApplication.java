8a48329f9ec8ed4298785295a595cddbefc8432d
Chandan Kumar Barik <barikchandan111@gmail.com>
2021-05-20 08:16:51 +0530

    -- Adding the new feature - to add Eureka Service Discovery

---
+++ AppDiscoveryService/src/main/java/com/appdiscovery/AppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ AppDiscoveryService/src/test/java/com/appdiscovery/AppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
